5dcc21e72df7435ce0ed6b9552edab2af43816a9
Maztumo <144551971+Maztumo@users.noreply.github.com>
2024-11-29 23:15:58 -0400

    He modificado a trayectorias de archivos y ramas por individual

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CriminalController;
+
+
 
 Route::get('/', [HomeController::class, "index"]);
 
-
-Route::get('/criminales', function () {
-    return "Historial de crimenales";
-});
+Route::get('/criminales/{criminal}/{mas?}', [CriminalController::class, "index"]);
 
 /* 
 Route::get('/criminales/{criminal}', function ($criminal) {
     return "Aquí aparece el historial del criminal {$criminal}.";
-}); AReglo
- */
-
-Route::get('/criminales/{criminal}/{mas?}', function ($criminal, $mas = null) {
-    if ($mas){
-        return "Aquí Aparece más informacion de: {$criminal} y relacion con {$mas}.";
-    }
-    return "Aquí aparece el historial del criminal {$criminal}.";
 });
+ */
