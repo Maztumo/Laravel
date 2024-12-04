1ecd6eb2fe1ca19f056eebe70a40c3d566c487b6
Maztumo <144551971+Maztumo@users.noreply.github.com>
2024-12-03 20:07:16 -0400

    Se hace unos cambios de ejemplo con yield. Tambien unos cambios en carpetas

---
+++ resources/views/pag/criminales.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1>Aquí aparece el historial de los criminales.</h1>
+</x-app-layout>
---
+++ resources/views/pag/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1> Bienvenido Agente</h1>    
+@endsection
